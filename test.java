5d0a11c13ba428c632943ef31c4a4b98da0b42c0
NY-Giants <Guest@wts9.attlocal.net>
2015-08-02 17:29:42 -0700

    hi

--- test.java
+++ test.java
@@ -1,4 +1,20 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 
 public class test {
-
+public static void main(String[] args)
+{
+	for(int i = 0; i<=100; i++){
+int randy = new Random().nextInt(1000);
+System.out.println(randy);
+if(randy %2 == 0){
+JOptionPane.showMessageDialog(null,"yes");
+}
+else{ 
+	JOptionPane.showMessageDialog(null, "no");
+}
+	}
+}
 }
